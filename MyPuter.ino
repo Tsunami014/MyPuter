742f9f9a4059f9ab5c1a39ef9415741b9d189a53
Tsunami014 (Max) <Tsunami014@duck.com>
2024-10-24 19:16:35 +1100

    Had a major restructure and now it works and yay!

--- MyPuter.ino
+++ MyPuter.ino
@@ -1,30 +1,96 @@
+// 0b0000
 int Writing = 50;
+int Reading = 50;
 
-int Address = 30;
-int AddrSize = 4;
+int ReadAddr = 34;
+int RAddrSize = 4;
+int WriteAddr = 30;
+int WAddrSize = 4;
 
-void write(int msg) {
-  for(int k=0; k < AddrSize; k++){
+int DataBus = 40;
+int DataSize = 8;
+
+void setwriteAddr(int msg) {
+  for(int k=0; k < WAddrSize; k++){
     int mask =  1 << k;
     int masked_n = msg & mask;
     int thebit = masked_n >> k;
-    digitalWrite(Address+k, thebit);
+    digitalWrite(WriteAddr+k, thebit);
   }
-  digitalWrite(Writing, LOW);
+}
+void setreadAddr(int msg) {
+  for(int k=0; k < RAddrSize; k++){
+    int mask =  1 << k;
+    int masked_n = msg & mask;
+    int thebit = masked_n >> k;
+    digitalWrite(ReadAddr+k, thebit);
+  }
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(Writing, OUTPUT);
+void writeData(int msg) {
+  for (int i = DataBus; i < DataBus+DataSize; i++) {
+    pinMode(i, OUTPUT);
+  }
+  for(int k=0; k < DataSize; k++){
+    int mask =  1 << k;
+    int masked_n = msg & mask;
+    int thebit = masked_n >> k;
+    digitalWrite(DataBus+k, thebit);
+  }
   digitalWrite(Writing, HIGH);
+}
+int readData() {
+  /*int data = 0;
+  for(int k=0; k < DataSize; k++){
+    data = data << 1;
+    data += digitalRead(DataBus+k);
+  }
+  return data;*/
+  digitalWrite(Reading, HIGH);
+}
 
-  for (int i = Address; i < Address+AddrSize; i++) {
-    pinMode(i, OUTPUT);  // Change to OUTPUT since you're writing to the pins
+void Reset() {
+  digitalWrite(Writing, LOW);
+  digitalWrite(Reading, LOW);
+  for (int i = DataBus; i < DataBus+DataSize; i++) {
+    pinMode(i, INPUT);
   }
 
-  write(0b0001);
+  /*for (int i = ReadAddr; i < ReadAddr+RAddrSize; i++) {
+    digitalWrite(i, LOW);
+  }
+  for (int i = WriteAddr; i < WriteAddr+WAddrSize; i++) {
+    digitalWrite(i, LOW);
+  }*/
+}
+
+int prev = 0;
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  pinMode(Writing, OUTPUT);
+  pinMode(Reading, OUTPUT);
+
+  for (int i = ReadAddr; i < ReadAddr+RAddrSize; i++) {
+    pinMode(i, OUTPUT);
+  }
+  for (int i = WriteAddr; i < WriteAddr+WAddrSize; i++) {
+    pinMode(i, OUTPUT);
+  }
+
+  Reset();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  setwriteAddr(0);
+  prev = prev + 1;
+  writeData(prev);
+  delay(500);
+  Reset();
+  setreadAddr(0);
+  readData();
+  Serial.println(prev);
+  delay(500);
 }
